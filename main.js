8d86e8e6e3d8cccab305db1fcd4a240ab0c8ce9f
Riya-Prasad <prasadriya02@gmail.com>
2023-09-08 09:09:04 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,159 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// nav link animation
+ScrollTrigger.create({
+  animation: gsap.from(".primary-nav li", {
+    yPercent: -250,
+    stagger: 0.3545,
+    duration: 1,
+    opacity: 0,
+  }),
+  trigger: ".primary-header",
+  start: "top 50%",
+  ease: "power3.easeOut",
+});
+
+
+// work horizontal scroll
+ScrollTrigger.matchMedia({
+  "(min-width:580px)": () => {
+    let horizontalSection = document.querySelector(".works");
+
+    gsap.to(".works", {
+      x: () => horizontalSection.scrollWidth * -1,
+      xPercent: 100,
+      scrollTrigger: {
+        trigger: ".works",
+        start: "center center",
+        end: "+=2000px",
+        pin: ".work-sec",
+        scrub: 2,
+        ease: "power4.easeInOut",
+        invalidateOnRefresh: true,
+      },
+    });
+  },
+});
+
+// works page work image reveal animation
+let revealContainers = document.querySelectorAll(".works-img");
+
+revealContainers.forEach((container) => {
+  let image = container.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      toggleActions: "restart none none reset",
+    },
+  });
+
+  tl.set(container, { autoAlpha: 1 });
+  tl.from(container, 1.5, {
+    xPercent: -100,
+    ease: Power2.out,
+  });
+  tl.from(image, 1.5, {
+    xPercent: 100,
+    scale: 1.2,
+    delay: -1.5,
+    ease: Power2.out,
+  });
+});
+
+
+// split text animation
+window.addEventListener("load", function () {
+  let revealText = document.querySelectorAll(".reveal-text");
+  let results = Splitting({ target: revealText, by: "lines" });
+
+  results.forEach((splitResult) => {
+    const wrappedLines = splitResult.lines
+      .map(
+        (wordsArr) => `
+        <span class="line"><div class="words">
+          ${wordsArr
+            .map(
+              (word) => `${word.outerHTML}<span class="whitespace"> 
+         </span>`
+            )
+            .join("")}
+        </div></span>`
+      )
+      .join("");
+    splitResult.el.innerHTML = wrappedLines;
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+  let revealLines = revealText.forEach((element) => {
+    const lines = element.querySelectorAll(".line .words");
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        toggleActions: "restart none none reset",
+        start: "top 80%",
+      },
+    });
+    tl.set(revealText, { autoAlpha: 1 });
+    tl.from(lines, 1, {
+      yPercent: 200,
+      ease: Power3.easeOut,
+      stagger: 0.1225,
+      delay: 0.3,
+    });
+  });
+});
+
+jQuery(document).ready(function () {
+  let splitLines = jQuery("[data-line]");
+  splitLines.each(function (i, j) {
+    let x = new SplitText(j, {
+      type: j.getAttribute("data-line"),
+      linesClass: "line",
+      wordsClass: "word",
+      charsClass: "char",
+    });
+    let delay = j.getAttribute("data-delay");
+
+    x.chars.forEach((ele, index) => {
+      if (delay) {
+        gsap.to(ele, { "--delay": index + parseInt(delay) + "s" });
+      } else {
+        gsap.to(ele, { "--delay": index + "s" });
+      }
+    });
+  });
+  let splitLines2 = jQuery("[data-line-no-animation]");
+  splitLines2.each(function (i, j) {
+    new SplitText(j, {
+      type: j.getAttribute("data-line-no-animation"),
+      linesClass: "line",
+      wordsClass: "word",
+      charsClass: "char",
+    });
+  });
+
+  let splitLines3 = jQuery("[data-line-within-line]");
+  splitLines3.each(function (i, j) {
+    let x = new SplitText(j, { type: "lines", linesClass: "line-inner" });
+    new SplitText(jQuery(j).find(".line-inner"), {
+      type: "lines",
+      linesClass: "line-mask",
+    });
+    let delay = j.getAttribute("data-delay");
+
+    x.lines.forEach((ele, index) => {
+      if (delay) {
+        let delaytamp = index + parseInt(delay);
+        gsap.to(ele, { "--delay": delaytamp + "s" });
+      } else {
+        gsap.to(ele, { "--delay": index + "s" });
+      }
+    });
+  });
+});
+
+jQuery(window).on("load", function () {
+  document.body.classList.add("loaded");
+});
+
